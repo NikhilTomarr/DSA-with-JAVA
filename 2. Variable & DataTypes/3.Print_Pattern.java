00829486d9e778c2de798fe3c808d13003b0d0e4
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-20 19:02:43 -0800

    Add Java program to print a descending pattern

---
+++ 2. Variable & DataTypes/3.Print_Pattern.java
@@ -0,0 +1,13 @@
+// ****   --- print this pattern
+// ***
+// **
+// *
+
+public class JavaBasics{
+  public static void main(String args[]){
+    System.out.println("****");
+    System.out.println("***");
+    System.out.println("**");
+    System.out.println("*");
+  }
+}
