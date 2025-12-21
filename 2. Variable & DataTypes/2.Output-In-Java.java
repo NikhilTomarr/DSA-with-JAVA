2fd29f0fbd1d04bfad47006db9f24ecb5b30aa35
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-20 18:55:22 -0800

    Add Java output examples in new file

---
+++ 2. Variable & DataTypes/2.Output-In-Java.java
@@ -0,0 +1,12 @@
+// 1. print -- System.out.print("HelloWorld");
+// 2. println -- System.out.println("HelloWorld"); 
+// 3. \n -- System.out.print("HelloWorld\n");
+
+
+public class JavaBasics{
+    public static void main(String[] args) {
+        System.out.print("Hello, World!"); 
+        System.out.println("Hello, World!"); // with line break
+        System.out.print("Hello, World!\n"); // with line break
+    }
+}
