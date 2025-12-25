353864d4a9994115646c7434906cb0623d0d01b0
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-24 23:38:23 -0800

    Add Java program to calculate area of a circle

---
+++ 2. Variable & DataTypes/10.Area_of_a_Circle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class JavaBasics{
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        Float rad = sc.nextFloat();
+        Float area = 3.12f*rad*rad;
+        System.out.println(area);
+
+    }
+}
