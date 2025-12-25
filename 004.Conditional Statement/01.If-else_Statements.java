52f16aad8cc4707ca1c7ab10fa6b6ca7b449ade8
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-25 00:59:01 -0800

    if-else statements with example voting age check
    Added a Java program to demonstrate if-else statements for voting eligibility based on age.

---
+++ 004.Conditional Statement/01.If-else_Statements.java
@@ -0,0 +1,21 @@
+// # IF-Else Satements
+// if(condtion){
+// }
+// else{
+// }
+
+//Example
+import java.util.Scanner;
+public class JavaBasics{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if(age>=18){
+            System.err.println("Allow To Vote");
+        }
+        else{
+            System.err.println("Not Allowed");
+        }
+        
+    }
+}
