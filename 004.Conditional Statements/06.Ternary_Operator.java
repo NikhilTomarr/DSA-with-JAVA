4768f9baeafc9d5e2b1b5ea3e8119f7229004e8b
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-25 02:01:14 -0800

    Ternary Operator with Example

---
+++ 004.Conditional Statements/06.Ternary_Operator.java
@@ -0,0 +1,11 @@
+// #Ternary Operator:
+// variable = (condition) ? statement1 : statement2;
+
+// For example:
+public class JavaBasics{
+    public static void main(String[] args) {
+        int number = 7;
+        String result = (number % 2 == 0) ? "Even" : "Odd";
+        System.out.println("The number is " + result);
+    }
+}
