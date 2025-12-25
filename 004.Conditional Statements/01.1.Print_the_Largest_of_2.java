1c88018522af0e77b189781116ed3616177f0461
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-25 01:08:03 -0800

    Add Java program to print the largest of two numbers

---
+++ 004.Conditional Statements/01.1.Print_the_Largest_of_2.java
@@ -0,0 +1,13 @@
+public class JavaBasics{
+    public static void main(String[] args) {
+        int A = 5;
+        int B = 10;
+        if(A>=B){
+            System.err.println("A is largest of 2");
+        }
+        else{
+            System.err.println("B is largest of 2");
+        }
+        
+    }
+}
