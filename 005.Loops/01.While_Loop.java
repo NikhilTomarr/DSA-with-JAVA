7f4e18a642c0fa7b2c41f138c55d2bdd1c31d34f
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-25 18:46:58 -0800

    While Loop with Example

---
+++ 005.Loops/01.While_Loop.java
@@ -0,0 +1,22 @@
+//#Loops: for, while, do-while
+
+//#while loop: A while loop repeatedly executes a block of code as long as a specified condition is true. 
+// The syntax is:
+// while (condition) {
+//     // code to be executed
+// }
+
+
+//For example:
+public class JavaBasics {
+    public static void main(String[] args) {
+        int counter =0;
+        while(counter < 100){
+            System.out.println("Hlooworld");
+            counter ++; 
+            
+        }
+        System.out.println("Completed");
+    }
+    
+}
