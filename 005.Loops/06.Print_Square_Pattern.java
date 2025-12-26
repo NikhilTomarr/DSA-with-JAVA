55cb113689b0838bd255c020a7e13279260e822d
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-25 19:25:02 -0800

    Print Square Pattern Using For Loop

---
+++ 005.Loops/06.Print_Square_Pattern.java
@@ -0,0 +1,14 @@
+//#Print Square Pattern Using For Loop
+//Output:
+//*********
+//*********
+//*********
+//*********
+
+public class JavaBasics{
+    public static void main(String[] args){
+        for(int line =1; line<=4; line++){
+            System.out.println("*********");
+        }
+    }
+}
