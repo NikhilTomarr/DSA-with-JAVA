c35d85c15a88f7e2bfe33bb4203b7cc4a45cc898
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-26 04:28:41 -0800

    Print Character Pattern Using Nested Loops

---
+++ 006.Nested Loops/04.Print_Character_Pattern.java
@@ -0,0 +1,20 @@
+// Print Character Pattern
+// A
+// BC
+// DEF
+// GHIJ
+
+public class JavaBasics {
+    public static void main(String[] args) {
+        int n = 4;
+        char ch = 'A';
+        for (int line = 1; line <= n; line++) {
+            for (int chars = 1; chars<=line; chars++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+        
+    }
+}
