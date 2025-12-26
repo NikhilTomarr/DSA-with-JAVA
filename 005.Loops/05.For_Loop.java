da54d7e0e4ebe35b911169c80c73058414e2e788
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-25 19:16:07 -0800

    For Loop with Example

---
+++ 005.Loops/05.For_Loop.java
@@ -0,0 +1,14 @@
+//#For Loop: A for loop in Java is a control flow statement that repeatedly executes a block of code a known number of times. It provides a concise way to manage the loop's initialization, condition checking, and iteration step in a single line. 
+// Syntax:
+// for (initialization; condition; updation) {
+//     // code block to be executed repeatedly
+// }
+
+//For Loop Example:
+public  class JavaBasics{
+    public static void main (String[] args){
+        for(int i=1; i<=5; i++){
+            System.out.println("Hello World");
+        }
+    }
+}
