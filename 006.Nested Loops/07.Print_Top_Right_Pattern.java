d76afdecbf4f8dc8200ffa2a75813661161f9803
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-26 07:54:50 -0800

    Print Top Right Pattern Using Nested Loops

---
+++ 006.Nested Loops/07.Print_Top_Right_Pattern.java
@@ -0,0 +1,23 @@
+// Print Top Right Pattern
+// *****
+//  ****
+//   ***
+//    **
+//     *
+
+
+public class JavaBasics{
+    public static void main(String[] args) {
+        int n = 5;
+        for(int row=1; row<=n; row++){
+            for(int col=1; col<=n; col++){
+                if(col-row>=0){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}    
