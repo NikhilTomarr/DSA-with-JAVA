10320f21a26cb52d3576f745f672763369459ff3
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-26 08:14:12 -0800

    Functions&Methods Syntax with Example

---
+++ 007.Functions & Methods/01.Syntax_of_Functions&Methods.java
@@ -0,0 +1,17 @@
+//#Functions & Methods : Block of code -> reusable
+
+// Syntax: 
+// returnType name(){
+//    //body
+//    return statement;
+// }
+
+// Example:
+public class JavaBasics {
+    public static void printHello(){
+        System.out.println("Hello");
+    }
+    public static void main(String[] args) {
+        printHello(); // function call
+    }
+}
