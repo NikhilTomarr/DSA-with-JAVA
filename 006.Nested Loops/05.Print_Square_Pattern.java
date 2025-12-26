993d09f5652a60213ed41c0ef5272331acda97a4
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-26 04:36:02 -0800

    Print Square Pattern Using Nested Loop

---
+++ 006.Nested Loops/05.Print_Square_Pattern.java
@@ -0,0 +1,19 @@
+// Print Square Pattern
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+
+
+public class JavaBasics {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
