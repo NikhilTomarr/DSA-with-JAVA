3864e90a548842d5ad3ba132c0f0e6c16c5dda87
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-26 04:17:13 -0800

    Print Inverted-Star Pattern Using Nested Loops

---
+++ 006.Nested Loops/02.Print_Inverted-Star_Pattern.java
@@ -0,0 +1,19 @@
+// Print the Inverted-Star pattern
+
+// * * * *
+// * * *        
+// * *
+// *
+
+public class JavaBasics {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1; i <= n; i++) {
+            for (int star = 1; star<=n-i+1; star++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
