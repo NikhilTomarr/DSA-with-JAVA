14dadbe2eb2ba04e95fbfe502f7abe98b65498ee
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-27 02:24:46 -0800

    find factorial of a number

---
+++ 007.Functions & Methods/05.Factorial_of_a_Number.java
@@ -0,0 +1,14 @@
+// Java program to find factorial of a number
+
+public class JavaBasics{
+    public static int factorial(int n){
+        int f=1;
+        for(int i=1; i<=n; i++){
+            f=f*i;
+        }
+        return f;
+    }
+    public static void main(String[] args){
+        System.out.println(factorial(4));
+    }
+} 
