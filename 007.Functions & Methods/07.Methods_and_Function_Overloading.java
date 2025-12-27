d1a83ce069bcf961cd61c4b74fe2d50c68ec84b2
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-27 02:50:04 -0800

    Methods and Function Overloading with Examples

---
+++ 007.Functions & Methods/07.Methods_and_Function_Overloading.java
@@ -0,0 +1,29 @@
+//#Methods:
+//     |
+//     |---User-defined Methods--->Exanmple: factorial,Sum,Product,etc.
+//     |
+//     |---Pre-defined Methods--->Example:Math(sqrt,pow,max) and sc.nextInt() etc.
+
+
+//#Function Overloading: Multiple functions with same name but different parameters.
+
+//1.Using Parameters:
+
+//f1: sum of 2 numbers :->
+int sum(int a, int b){
+    return a + b;
+}
+
+//f2: sum of 3 numbers
+int sum(int a, int b, int c){
+    return a + b + c;
+}
+
+//2.Using Data Types:
+//f1: add 2 int values :->
+int add(int a, int b){
+    return a + b;
+}
+
+//f2: add 2 float values :->
+    
