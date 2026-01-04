a3d686670f260b0eef4236cea17f2f36c6775937
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2026-01-03 19:16:41 -0800

    Scope Definition

---
+++ 007.Functions & Methods/12.Scope.java
@@ -0,0 +1,12 @@
+// #Scope
+
+// 1.Method Scope: Variables declared within a method/function are only accessible within that method/function.
+
+// 2.Block Scope: Variables declared within a block (e.g., inside loops or conditionals) are only accessible within that block.
+
+// par agar {} ka bhar variable declare karte hai to wo {} ka andar or bhar use kar sakta ha.
+
+// Example:
+// int p = 15;
+// {System.out.print(p);}
+// System.out.print(p);
