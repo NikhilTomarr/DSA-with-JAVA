f1d7728110dc5a4188123532fb29e2ce3d6a0817
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2026-01-04 03:47:58 -0800

    Creating an Array

--- 008.Arrays/02.Creating_an_Array.java
+++ 008.Arrays/02.Creating_an_Array.java
@@ -1,1 +1,8 @@
+// dataType array[] = new dataType[size];
 
+// Creating an Array
+
+// int marks[] = new int[50];
+// int numbers[] = {1, 2, 3, 4, 5};
+// int moreNumbers[] = {10, 20, 30, 40, 50};
+// String fruits[] = {"Apple", "Banana", "Cherry"};
