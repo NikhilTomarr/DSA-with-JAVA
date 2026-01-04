1836dbac5af7c029a005ce29f725c63b76dd64f0
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2026-01-03 18:45:34 -0800

    Print All Primes in a Range

---
+++ 007.Functions & Methods/09.Print_All_Primes_in_a_Range.java
@@ -0,0 +1,22 @@
+public class JavaBasics{
+    public static boolean isPrime (int n){
+        for(int i = 2; i<=Math.sqrt(n); i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void primesInRange(int n){
+        for(int i = 2; i<=n; i++){
+            if (isPrime(i)) { //true
+                System.out.print(i+" ");
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        primesInRange(20);
+    }
+}
