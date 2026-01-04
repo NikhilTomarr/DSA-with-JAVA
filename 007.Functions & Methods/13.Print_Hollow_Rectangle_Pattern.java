eee2d2ade17360d761a638c6ae153b72fb6a62b3
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2026-01-03 19:29:59 -0800

    Print Hollow Rectangle Pattern

---
+++ 007.Functions & Methods/13.Print_Hollow_Rectangle_Pattern.java
@@ -0,0 +1,17 @@
+public class JavaBasics {
+    public static void hallow_rectangle(int rows, int cols){
+        for(int i = 1; i <= rows; i++){
+            for(int j = 1; j <= cols; j++){
+                if(i==1 || i==rows || j==1 || j==cols){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        hallow_rectangle(4, 5);
+    }
+}
