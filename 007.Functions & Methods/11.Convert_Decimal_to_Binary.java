ed7018c1706184eab456a2014116e27c3809f20e
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2026-01-03 19:06:59 -0800

    Convert Decimal to Binary

---
+++ 007.Functions & Methods/11.Convert_Decimal_to_Binary.java
@@ -0,0 +1,17 @@
+public class JavaBasics {
+    public static void decToBin(int n){
+        int myNum = n;
+        int binNum = 0;
+        int pow = 0;
+        while(n>0){
+            int rem = n % 2;
+            binNum = binNum + (rem * (int)Math.pow(10, pow));
+            pow++;
+            n = n / 2;
+        }
+        System.out.println("Decimal of " + myNum + " is: " + binNum);
+    }
+    public static void main(String[] args) {
+        decToBin(15);
+    }
+}
