216b63de66ef0220a6b65ac0a4ee827d9f10b460
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2026-01-04 23:33:27 -0800

    Pairs in an Array

---
+++ 008.Arrays/10.Pairs_in_an_Array.java
@@ -0,0 +1,26 @@
+// #Pairs in an Array
+//  ______________________
+// | 2 | 4  | 6 | 8  | 10 |
+// '----------------------'
+// Output:
+// (2,4) (2,6) (2,8) (2,10)
+// (4,6) (4,8) (4,10)
+// (6,8) (6,10)
+// (8,10)
+
+public class JavaBasics{
+    public static void printPairs(int numbers[]){
+       for(int i=0; i<numbers.length; i++){
+        int curr = numbers[i]; //2, 4, 6, 8,10
+           for(int j= i+1; j<numbers.length; j++){
+               System.out.print("("+curr+","+numbers[j]+") ");
+           }
+           System.out.println();
+       }
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2,4,6,8,10};
+        printPairs(numbers);
+    }
+}
